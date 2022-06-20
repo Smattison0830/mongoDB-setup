b7ce3e265c1d2d554e8ed1640de04316f6d01962
Spencer Mattison <spenceramattison@gmail.com>
2022-06-20 18:20:47 +0900

    Write code to createProduct

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+database = require("./mongo");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/products');
+app.post("/products", database.createProduct);
 
-app.get('/products');
+app.get("/products");
 
 app.listen(3000);
--- mongo.js
+++ mongo.js
@@ -1,4 +1,27 @@
-const createProduct = async (req, res, next) => {};
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const url = process.env.DB;
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  }
+
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
